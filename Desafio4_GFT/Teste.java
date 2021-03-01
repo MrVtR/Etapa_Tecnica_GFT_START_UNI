3ccc1179d8f56a83253a3149fad7afeb324d4914
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-03-01 11:01:44 -0300

    Ajustes feitos no Desafio 4

--- Desafio4_GFT/Teste.java
+++ Desafio4_GFT/Teste.java
@@ -10,12 +10,13 @@ package desafio4_gft;
  * @author vitor
  */
 public class Teste {
+
     public static void main(String[] args) {
         // TODO code application logic here
         Quadrado q = new Quadrado(5);
         Retangulo r = new Retangulo(5, 7);
         Circulo c = new Circulo(4);
-        
+
         System.out.println("Área do Quadrado: " + q.calcularArea());
         System.out.println("Área do Retângulo: " + r.calcularArea());
         System.out.println("Área do Círuclo: " + c.calcularArea());
