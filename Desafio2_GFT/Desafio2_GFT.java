4898842041d56e7b4ca5089e156aac72d8f337e3
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-03-01 10:57:33 -0300

    Ajustes feitos no Desafio 2

--- Desafio2_GFT/Desafio2_GFT.java
+++ Desafio2_GFT/Desafio2_GFT.java
@@ -16,9 +16,11 @@ public class Desafio2_GFT {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        for(int i = 10; i<215;i++)
-            if(i%3==0 && i%7==0)
+        System.out.println("Números divisíveis por 3 e 7:");
+        for (int i = 10; i < 215; i++) {
+            if (i % 3 == 0 && i % 7 == 0) {
                 System.out.println(i);
+            }
+        }
     }
-    
 }
